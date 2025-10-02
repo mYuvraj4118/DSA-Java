ac644b58f2ecec0f98afe9b1d4fb9016b0d94585
Yuvraj Mathe <matheyuvraj@gmail.com>
2025-10-02 23:58:48 +0530

    Prints hollo rectangle pattern

---
+++ HollowRectPattern.java
@@ -0,0 +1,11 @@
+public class HollowRectPattern {
+    public static void hollo_rectangle(int totRows, int totCols){
+        for(int i =1; i <= totRows; i++){
+            for(int j = 1; j <= totCols j++){
+                if( i == 1 || i == totRows || j == totCols){
+                    
+                }
+            }
+        }
+    }
+}
