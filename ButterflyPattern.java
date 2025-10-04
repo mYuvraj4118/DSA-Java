7f82253390f6c6fa48f1a0ba12856f479531ac31
Yuvraj Mathe <matheyuvraj@gmail.com>
2025-10-04 18:13:49 +0530

    Prints butterfly pattern

---
+++ ButterflyPattern.java
@@ -0,0 +1,46 @@
+// Program to print butterfly pattern using stars in java
+
+import java.util.*;
+
+public class ButterflyPattern {
+    public static void butterfly(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            int spaces = 2 * (n - i);
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENnter the size of the pattern: ");
+        int size = sc.nextInt();
+        butterfly(size);
+    }
+}
