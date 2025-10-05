af03ba1cb9fbfd10e1f60f31522db43e6492666e
Yuvraj Mathe <matheyuvraj@gmail.com>
2025-10-05 22:54:21 +0530

    Prints diamond shape pattern

---
+++ DiamondPattern.java
@@ -0,0 +1,39 @@
+/Program to print diamond pattern using stars in java
+
+import java.util.Scanner;
+
+public class DiamondPattern {
+    public static void diamond(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the diamond: ");
+        int size = sc.nextInt();
+        sc.close();
+        diamond(size);
+    }
+}
