3dc83358f83216ff57a2e49db63da307070f95bb
Yuvraj Mathe <matheyuvraj@gmail.com>
2026-01-03 18:05:10 +0530

    Calculates total sum of diagonal elements in 2D array

---
+++ DiagonalSum.java
@@ -0,0 +1,29 @@
+public class DiagonalSum {
+
+    public static int printdiagonalSum(int matrix[][]) {
+
+        int sum = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            // Primary Diagonal
+            sum += matrix[i][i];
+
+            // Secondary diagonal
+            if (i != matrix.length - 1 - i) {
+                sum += matrix[i][matrix.length - i - 1];
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        int matrix[][] = { { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 } };
+
+        System.out.println(printdiagonalSum(matrix));
+    }
+}
