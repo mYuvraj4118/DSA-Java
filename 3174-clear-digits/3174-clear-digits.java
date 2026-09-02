be40a5ad4cefb7fdeecaaae4ed09590e6587284c
Yuvraj Mathe <matheyuvraj@gmail.com>
2026-09-02 23:34:53 +0530

    Time: 1 ms (99.92%), Space: 43.4 MB (97.15%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.java
+++ 3174-clear-digits/3174-clear-digits.java
@@ -4,12 +4,10 @@ class Solution {
 
         for (char ch : s.toCharArray()) {
             if (Character.isDigit(ch)) {
-                // Remove closest non-digit character to the left
                 if (stack.length() > 0) {
                     stack.deleteCharAt(stack.length() - 1);
                 }
             } else {
-                // Add non-digit character
                 stack.append(ch);
             }
         }
